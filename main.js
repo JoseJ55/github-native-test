d58453dede5b3b973434a39dd4139d2b6405c004
Jose Jimenez <josejimenez826@gmail.com>
2022-08-27 11:48:00 -0700

    change main

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-const inital = "svondsa feouhdsiornhv; jvfn;e lble bfbebg  dbbboe fnrogveo ao bnnbovr aonveorufnh e b ;";
+const inital = "svondg dghnb dgfndsa feouhdsiornhv; jvfn;e lble bfbebg  dbbboe fnrogveo ao bnnbovr aonveorufnh e b ;";
 
 console.log(inital);
