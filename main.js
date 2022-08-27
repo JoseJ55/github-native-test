bddab342daf81135752a14e69773e32df9c2443c
Jose Jimenez <josejimenez826@gmail.com>
2022-08-27 11:41:09 -0700

    chagne main

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-const inital = "svondsa feonbovr aonveorufnh e b ;";
+const inital = "svondsa feouhdsiornhv; jvfn;e lble bfbebg  dbbboe fnrogveo ao bnnbovr aonveorufnh e b ;";
 
 console.log(inital);
